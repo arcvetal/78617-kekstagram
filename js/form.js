62ff038c10f87e395ae72e7d467f7b887598b6a6
arcvetal <arcvetal@gmail.com>
2017-02-05 17:03:23 +0300

    Перевесил обрабртчик. Сделал табы

--- js/form.js
+++ js/form.js
@@ -5,26 +5,23 @@ var cropping = document.querySelector('.upload-overlay');
 var download = document.querySelector('#upload-select-image');
 var btnClose = document.querySelector('.upload-form-cancel');
 var photo = document.querySelector('.filter-image-preview');
+var filterControls = document.querySelector('.upload-filter-controls');
 
 var scaleWindow = document.querySelector('.upload-resize-controls-value');
 var resizeBtnIncr = document.querySelector('.upload-resize-controls-button-inc');
 var resizeBtnDecr = document.querySelector('.upload-resize-controls-button-dec');
 
 var filters = document.querySelectorAll('input[name=upload-filter]');
+var filterLabels = document.querySelectorAll('.upload-filter-label');
 
-function clickFilter(filter) {
-  filter.addEventListener('click', function () {
-    toggleFilter(filter);
-  });
-}
-
-function toggleFilter(filter) {
+function toggleFilter(event) {
   for (i = 0; i < filters.length; i++) {
     photo.classList.remove('filter-' + filters[i].value);
   }
 
-  photo.classList.add('filter-' + filter.value);
+  photo.classList.add('filter-' + event.target.value);
 }
+
 
 function makeTransform() {
   for (i = 1; i < 4; i++) {
@@ -34,11 +31,29 @@ function makeTransform() {
   photo.classList.add('transform' + scaleWindow.value);
 }
 
-for (var i = 0; i < filters.length; i++) {
-  clickFilter(filters[i]);
-}
+filterControls.addEventListener('click', function () {
+  toggleFilter(event);
+});
+
+filterControls.addEventListener('keydown', function (event) {
+  if (event.keyCode === 13) {
+    toggleFilter(event);
+  }
+});
 
 scaleWindow.setAttribute('value', 100);
+
+//  Добавим фильтрам атрибут ТАБИНДЕКС!!!
+
+filterControls.setAttribute('tabindex', '0');
+
+for (var i = 0; i < filterLabels.length; i++) {
+  setTabIndex(filterLabels[i], i);
+}
+
+function setTabIndex(filterLabel, n) {
+  filterLabel.setAttribute('tabindex', 0);
+}
 
 resizeBtnDecr.addEventListener('click', function () {
   if (scaleWindow.value > 25) {
