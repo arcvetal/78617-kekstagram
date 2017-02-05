b42dbc4a57b1c6f4bc7ac8bbb03a7580ae781455
arcvetal <arcvetal@gmail.com>
2017-02-05 23:45:06 +0300

    Табы заработали !

--- js/form.js
+++ js/form.js
@@ -3,40 +3,48 @@
 var uploadFile = document.querySelector('#upload-file');
 var cropping = document.querySelector('.upload-overlay');
 var download = document.querySelector('#upload-select-image');
-var btnClose = document.querySelector('.upload-form-cancel');
-var photo = document.querySelector('.filter-image-preview');
-var filterControls = document.querySelector('.upload-filter-controls');
+var btnClose = cropping.querySelector('.upload-form-cancel');
+var photo = cropping.querySelector('.filter-image-preview');
+var filterControls = cropping.querySelector('.upload-filter-controls');
 
-var scaleWindow = document.querySelector('.upload-resize-controls-value');
-var resizeBtnIncr = document.querySelector('.upload-resize-controls-button-inc');
-var resizeBtnDecr = document.querySelector('.upload-resize-controls-button-dec');
+var scaleWindow = cropping.querySelector('.upload-resize-controls-value');
+var resizeBtnIncr = cropping.querySelector('.upload-resize-controls-button-inc');
+var resizeBtnDecr = cropping.querySelector('.upload-resize-controls-button-dec');
 
-var filters = document.querySelectorAll('input[name=upload-filter]');
-var filterLabels = document.querySelectorAll('.upload-filter-label');
+var filters = filterControls.querySelectorAll('input[name=upload-filter]');
+var filterLabels = filterControls.querySelectorAll('.upload-filter-label');
 
 function toggleFilter(event) {
   for (i = 0; i < filters.length; i++) {
     photo.classList.remove('filter-' + filters[i].value);
   }
-
-  photo.classList.add('filter-' + event.target.value);
+  if (event.target.htmlFor) {
+    var inputId = event.target.htmlFor;
+    var filterInput = filterControls.querySelector('#' + inputId);
+    photo.classList.add('filter-' + filterInput.value);
+  } else {
+    if (event.target.checked) {
+      photo.classList.add('filter-' + event.target.value);
+    }
+  }
 }
-
 
 function makeTransform() {
   for (i = 1; i < 4; i++) {
     photo.classList.remove('transform' + 25 * i);
   }
-
   photo.classList.add('transform' + scaleWindow.value);
 }
 
-filterControls.addEventListener('click', function () {
+filterControls.addEventListener('click', function (event) {
   toggleFilter(event);
 });
 
 filterControls.addEventListener('keydown', function (event) {
   if (event.keyCode === 13) {
+    var inputId = event.target.htmlFor;
+    var filterInput = filterControls.querySelector('#' + inputId);
+    filterInput.checked = true;
     toggleFilter(event);
   }
 });
